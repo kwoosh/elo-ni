778b1387d21b0717f2b9a6a3869dc4f6873ab942
Andrew Pashinnik <tobirawork@gmail.com>
2018-05-27 14:50:49 +0300

    feat(components): add Clock component

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import './Clock.css'
+
+class Clock extends Component {
+    render() {
+        const degree = 360 / 60 * this.props.seconds
+
+        return (
+            <article className="clock simple">
+                <div
+                    className="seconds-container"
+                    style={{
+                        transform: `rotateZ(${degree}deg)`,
+                    }}
+                >
+                    <div className="seconds" />
+                </div>
+            </article>
+        )
+    }
+}
+
+export default Clock
