2d52772cf5ebd73578c9c44a55dd88981a90a933
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-02 11:27:43 +0300

    feat(App): add tabs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-// @flow
 import React, { Component } from 'react'
 import Stopwatch from './components/Stopwatch/Stopwatch'
+import Snippets from './components/Snippets/Snippets'
 
-class App extends Component<{}> {
+class App extends Component {
+    tabs = ['Stopwatch', 'Snippets']
+
+    state = {
+        screen: 'stopwatch',
+        snippets: [],
+    }
+
+    toggleTab = tab => () => {
+        this.setState(prev => ({ screen: tab.toLowerCase() }))
+    }
+
+    handleSnippetCreation = snippet => {
+        console.log('From app:', snippet)
+        this.setState(({ snippets }) => ({ snippets: [...snippets, snippet] }))
+    }
+
     render() {
         return (
             <div className="App">
-                <Stopwatch />
+                <ul className="App__tabs">
+                    {this.tabs.map((tab, i) => (
+                        <li key={i} onClick={this.toggleTab(tab)}>
+                            {tab}
+                        </li>
+                    ))}
+                </ul>
+
+                <Snippets visible={this.state.screen === 'snippets'} />
+                <Stopwatch visible={this.state.screen === 'stopwatch'} onSippet={this.handleSnippetCreation} />
             </div>
         )
     }
