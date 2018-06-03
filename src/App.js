e65ec64aa7be465f92cac749bc0f2509b6f7736a
Andrew Pashinnik <tobirawork@gmail.com>
2018-06-03 14:08:23 +0300

    feat(Stopwatch): add input and lastSnippetId

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import Stopwatch from './components/Stopwatch/Stopwatch'
 import Snippets from './components/Snippets/Snippets'
 
@@ -10,16 +11,19 @@ class App extends Component {
         snippets: [],
     }
 
+    getLastSnippetId = arr => arr.reduce((prev, curr) => (curr > prev ? curr : prev), 0)
+
     toggleTab = tab => () => {
         this.setState(prev => ({ screen: tab.toLowerCase() }))
     }
 
     handleSnippetCreation = snippet => {
-        console.log('From app:', snippet)
         this.setState(({ snippets }) => ({ snippets: [...snippets, snippet] }))
     }
 
     render() {
+        const lastSnippetId = this.getLastSnippetId(this.state.snippets.map(({ id }) => id))
+
         return (
             <div className="App">
                 <ul className="App__tabs">
@@ -31,7 +35,11 @@ class App extends Component {
                 </ul>
 
                 <Snippets visible={this.state.screen === 'snippets'} />
-                <Stopwatch visible={this.state.screen === 'stopwatch'} onSippet={this.handleSnippetCreation} />
+                <Stopwatch
+                    visible={this.state.screen === 'stopwatch'}
+                    onSippet={this.handleSnippetCreation}
+                    lastSnippetId={lastSnippetId}
+                />
             </div>
         )
     }
--- src/components/Stopwatch/Stopwatch.js
+++ src/components/Stopwatch/Stopwatch.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import Circle from '../Circle/Circle'
-import Button from '../Button/Button'
-import './Stopwatch.css'
 import dayjs from 'dayjs'
+
+import Button from '../Button/Button'
+import Circle from '../Circle/Circle'
+import Input from '../Input/Input'
+
+import './Stopwatch.css'
 
 class Stopwatch extends Component {
     state = {
+        snippetMassage: '',
         milliseconds: 0,
     }
 
-    constructor(props: Props) {
+    constructor(props) {
         super(props)
 
         this.interval = null
@@ -32,11 +36,18 @@ class Stopwatch extends Component {
         this.setState({ milliseconds: 0 })
     }
 
+    handleSnippetInput = e => {
+        const { value } = e.target
+
+        this.setState(prev => ({ snippetMassage: value }))
+    }
+
     createSnippet = (time, massage) => () => {
         const snippet = {
-            massage,
-            inHours: Number((time.mins / 60).toFixed(2)),
+            id: this.props.lastSnippetId + 1,
             when: dayjs().format('DD MMMM in HH:mm:ss'),
+            inHours: Number((time.mins / 60).toFixed(2)),
+            massage,
         }
 
         this.props.onSippet(snippet)
@@ -71,7 +82,11 @@ class Stopwatch extends Component {
                     <Button onClick={this.start} text="start" />
                     <Button onClick={this.stop} text="stop" />
                     <Button onClick={this.clear} text="clear" />
-                    <Button onClick={this.createSnippet(time)} text="snippet" />
+                </div>
+
+                <div className="Stopwatch__snippet-form">
+                    <Input type="text" placeholder="massage" onInput={this.handleSnippetInput} />
+                    <Button onClick={this.createSnippet(time, this.state.snippetMassage)} text="snippet" />
                 </div>
             </div>
         )
